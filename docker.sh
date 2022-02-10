b1142536d8a8bc763bed1f48a02907c559951493
davenkat <davenkatesh56@gmail.com>
2022-02-10 15:29:56 +0000

    1: Activity 1 Changes

---
+++ docker.sh
@@ -0,0 +1,19 @@
+#Docker Installation Commands
+
+curl -fsSL https://get.docker.com -o get-docker.sh #Always installs latest version of docker
+sh get-docker.sh
+sudo usermod -aG docker <username> #$USER
+docker info
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+# Creation of Containers
+
+docker image ls #check any images present in local repository
+
+#Downloading images from Registry or docker hub
+docker pull <image_name>:<tag> #image name represents application , Tag represents version by default docker considers latest version
+
+ex: docker pull jenkins/jenkins 
+
+#Docker File
